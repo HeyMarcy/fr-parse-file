2cca11571ba849c58bcc665dabd596c0ad93644b
Marcy Montross <marcy.montross@gmail.com>
2020-01-23 11:56:53 -0600

    adds uuid and mock data.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import FileInputForm from './FileInputForm'
+import stlData from '../stl-data.json'
+import { v4 } from 'uuid'
 
 function App() {
+    const [files, setFiles] = useState(stlData)
     return (
         <div className="App">
-            <FileInputForm />
+            <FileInputForm
+                onNewFile={ascii => {
+                    const newFiles = [
+                        ...files,
+                        {
+                            id: v4(),
+                            rating: 0,
+                            ascii,
+                        },
+                    ]
+                    setFiles(newFiles)
+                }}
+            />
         </div>
     )
 }
